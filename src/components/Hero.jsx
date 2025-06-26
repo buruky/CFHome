87100d14f00c235881ff5f1484fbe3af3d740604
buruk yimesgen <buruky@uw.edu>
2025-06-25 23:15:04 -0700

    da

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
       <div className="w-full h-full bg-black/50 flex items-center">
         <div className="grid md:grid-cols-2 max-w-[1240px] m-auto px-4 text-white">
           <div className="flex flex-col justify-center md:items-start w-full py-8">
-            <p className="text-2xl">Exceptional Se ior Care</p>
+            <p className="text-2xl">Exceptional Senior Care</p>
             <h1 className="py-3 text-5xl md:text-7xl font-bold">Always with Compassion</h1>
             <p className="text-2xl">Comfort, dignity, and expert care for your loved ones.</p>
           </div>
